f4451ff8d109ec1ffa61f8e6113853c1b0dba4a1
Michal Brzozowski <miki@extremic.com>
2017-11-03 21:23:04 +0100

    Add tests

---
+++ run_tests.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+
+for I in tests/*; do 
+  echo "Running $I"
+  ./zygmunt $I > tmp.cpp 2> log.out
+  if [ "$?" != "0" ]; then
+    echo "$RED Compilation failed$NC"
+    continue
+  fi
+  g++ -std=c++14 tmp.cpp -o tmp
+  if [ "$?" != "0" ]; then
+    echo "$RED C++ compilation failed$NC"
+    continue
+  fi
+  ./tmp
+  RESULT=$?
+  EXPECTED=`head -n 1 $I | cut -c 4-`
+  if [ "$RESULT" != "$EXPECTED" ]; then
+    echo "$RED Expected $EXPECTED, got $RESULT$NC"
+    continue
+  fi
+  echo "$GREEN Success$NC"
+done
+
